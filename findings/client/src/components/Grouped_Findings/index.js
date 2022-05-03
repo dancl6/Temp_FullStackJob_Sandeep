91cbecb2939518cc04444fb4e8a0041c7d4bc413
Dan Lyons <danielclyons@yahoo.com>
2022-05-03 13:43:33 -0400

    got table working

--- findings/client/src/components/Grouped_Findings/index.js
+++ findings/client/src/components/Grouped_Findings/index.js
@@ -1,8 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { Dropdown } from "react-bootstrap"
+import Select from 'react-select';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from 'react-bootstrap/Table'
+
+// import styled, { css } from 'styled-components'
 function Grouped_Findings() {
+
+    const actions = [
+        { label: "Add", value: 1 },
+        { label: "Edit", value: 2 },
+        { label: "Delete", value: 3 }
+      ];
+
   const [dataResponse, setDataResponse ] = useState()
+  
+
     useEffect(() => {
         axios.get('/grouped_findings').then(response=> {
           console.log("grouped findings response from axios is:", response)
@@ -19,29 +32,54 @@ function Grouped_Findings() {
         })
     }, []);
     return (
-      <div>
-        
-        <Dropdown>
-  <Dropdown.Toggle variant="success" id="dropdown-basic">
-    Dropdown Button
-  </Dropdown.Toggle>
 
-  <Dropdown.Menu>
-    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-  </Dropdown.Menu>
-</Dropdown>
 
-        <div className="App">
-         At Grouped Findings
-         {dataResponse? 
-          <div>{dataResponse.data.data[0].severity}</div>
-        : <div></div>}
-         
-        </div>
-      </div>
-      );
+<Table striped bordered hover>
+  <thead>
+    <tr>
+
+      <th>Severity</th>
+      <th>Time</th>
+      <th>SLA</th>
+      <th>Description</th>
+      <th>Security Analyst</th>
+      <th>Owner</th>
+      <th>Status</th>
+      <th># Findings</th>
+      <th>Communications</th>
+      <th>Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+
+  { 
+            dataResponse?.data.data.map(item =>
+            item  ?
+            (
+
+    <tr>
+      <td>{item.severity}</td>
+      <td>{item.grouped_finding_created}</td>
+      <td>{item.sla}</td>
+      <td>{item.description}</td>
+      <td>{item.security_analyst}</td>
+      <td>{item.owner}</td>
+      <td>{item.workflow}</td>
+      <td>{item.status}</td>
+      <td>test</td>
+      <td>test</td>
+
+    </tr>
+  
+
+            ): null)}
+  </tbody>
+
+{/* </table> */}
+</Table>
+    )
+
+
     }
     
     export default Grouped_Findings;
