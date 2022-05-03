7269d6e16552e977f553a28fd075ae8dcc3a067d
Dan Lyons <danielclyons@yahoo.com>
2022-05-03 10:53:24 -0400

    implemented pie chart using grouped findings data

--- findings/client/src/components/Pie_Chart/index.js
+++ findings/client/src/components/Pie_Chart/index.js
@@ -1,18 +1,55 @@
-// import { PieChart } from 'react-minimal-pie-chart';
-// import { AgChartsReact } from "ag-charts-react";
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import Chart from 'react-google-charts'
 function Pie_Chart() {
 
+    const [dataResponse, setDataResponse ] = useState()
+    useEffect(() => {
+        axios.get('/grouped_findings').then(response=> {
+          console.log("grouped findings response from axios is:", response)
+
+          setDataResponse(response)
+        })
+
+    }, []);
+
+var critical, high, medium, low 
+critical = 0
+high = 0
+medium = 0
+low = 0
+if (dataResponse) {
+    for ( let i = 0; i < dataResponse.data.data.length; i ++ ) {
+        if (dataResponse.data.data[i].severity === "critical" ){
+            critical = critical + 1
+        }
+        if (dataResponse.data.data[i].severity === "high" ){
+            high = high + 1
+        }
+        if (dataResponse.data.data[i].severity === "medium" ){
+            medium = medium + 1
+        }
+        if (dataResponse.data.data[i].severity === "low" ){
+            low = low + 1
+        }
+    }
+    critical = critical/dataResponse.data.data.length*100
+    high = high/dataResponse.data.data.length*100
+    medium = medium/dataResponse.data.data.length*100
+    low = low/dataResponse.data.data.length*100
+
+console.log("critical:", critical, "high:", high, "medium:", medium, "low", low)
+}
     const pieData = [
-        ['Task', 'Hours per Day'],
-        ['Work', 11],
-        ['Eat', 2],
-        ['Commute', 2],
-        ['Watch TV', 2],
-        ['Sleep', 7],
+        ['Severity', 'Percent'],
+        ['Critical', critical],
+        ['High', high],
+        ['Medium', medium],
+        ['Low', low],
+        
       ]
       const pieOptions = {
-        title: 'My Daily Activities',
+        title: 'Grouped Findings By Severity',
         pieHole: 0.4,
       }
 
